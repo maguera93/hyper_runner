3916e41d31e85ea7b732a7e6885eb3f0219ec463
maguera <magueracoras@gmail.com>
2021-11-10 17:27:22 +0100

    UI implementation

--- Assets/Scripts/UI/EndPopup.cs
+++ Assets/Scripts/UI/EndPopup.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MAG.Popups;
 
-public class EndScreen : MonoBehaviour
+public class EndScreen : PopupStateModel<EndScreen, PlayerModel>
 {
+
+
+    public override void Setup(PlayerModel playerModel)
+    {
+        base.Setup(playerModel);
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/UI/WinCondition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct WinCondition
+{
+    public int SCondition;
+    public int ACondition;
+    public int BCondition;
+    public int CCondition;
+}
